9abbc27d463151c30959adef3baa4e8a57b7f15e
Agno <agnodev87@gmail.com>
2023-09-07 15:22:29 -0300

    first commit

---
+++ src/main/java/br/com/arenagym/alunotreino/AlunoTreinoApplication.java
@@ -0,0 +1,13 @@
+package br.com.arenagym.alunotreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoTreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoTreinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arenagym/alunotreino/AlunoTreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arenagym.alunotreino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoTreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
