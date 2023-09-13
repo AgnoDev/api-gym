d541b93491d78988ade2674da5ac2513ca0113dc
Agno <agnodev87@gmail.com>
2023-09-13 20:26:23 -0300

    utiliza stream().map() pra converter dominio em response

--- src/main/java/br/com/arenagym/alunotreino/aluno/application/api/AlunoListResponse.java
+++ src/main/java/br/com/arenagym/alunotreino/aluno/application/api/AlunoListResponse.java
@@ -1,11 +1,14 @@
 package br.com.arenagym.alunotreino.aluno.application.api;
 
 import br.com.arenagym.alunotreino.aluno.domain.Aluno;
+import lombok.Value;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public class AlunoListResponse {
+@Value
+public class AlunoListResponse { // essa é a resposta que vai pro front
 
     private UUID idAluno;
     private String nomeCompleto;
@@ -13,7 +16,17 @@ public class AlunoListResponse {
     private String email;
     private String celular;
 
+    public AlunoListResponse(Aluno aluno) { //esse cara alimenta a resposta pro front
+        this.idAluno = aluno.getIdAluno();
+        this.nomeCompleto = aluno.getNomeCompleto();
+        this.cpf = aluno.getCpf();
+        this.email = aluno.getEmail();
+        this.celular = aluno.getCelular();
+    }
+
     public static List<AlunoListResponse> converte(List<Aluno> alunos) {
-        return null;
+        return alunos.stream()      //põe a lista de alunos dominio pra rodar
+                .map(AlunoListResponse::new)    //transforma em alunos response
+                .collect(Collectors.toList());  // cria nova lista com alunos response
     }
 }
