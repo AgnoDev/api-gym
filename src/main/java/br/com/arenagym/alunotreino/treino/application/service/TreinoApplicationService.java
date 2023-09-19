16741efa18de20623c35a3ac6d01c5ed43d1928a
Agno <agnodev87@gmail.com>
2023-09-19 18:00:01 -0300

    cria construtor para Treino(DTO) para persistencia no DB

--- src/main/java/br/com/arenagym/alunotreino/treino/application/service/TreinoApplicationService.java
+++ src/main/java/br/com/arenagym/alunotreino/treino/application/service/TreinoApplicationService.java
@@ -1,7 +1,11 @@
 package br.com.arenagym.alunotreino.treino.application.service;
 
+import br.com.arenagym.alunotreino.aluno.application.service.AlunoService;
+import br.com.arenagym.alunotreino.treino.application.api.TreinoAPI;
 import br.com.arenagym.alunotreino.treino.application.api.TreinoRequest;
 import br.com.arenagym.alunotreino.treino.application.api.TreinoResponse;
+import br.com.arenagym.alunotreino.treino.domain.Treino;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +13,18 @@ import java.util.UUID;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class TreinoApplicationService implements TreinoService {
+    private final AlunoService alunoService;
+    private final TreinoRepository treinoRepository;
+
     @Override
     public TreinoResponse postTreino(UUID idAluno, TreinoRequest treinoRequest) {
         log.info("[start] -> TreinoApplicationService -> postTreino");
         log.info("[idAluno] {}", idAluno);
-       //] TreinoResponse treino = treinoService.postTreino(idAluno, treinoRequest);
+        alunoService.getAlunoById(idAluno);
+        Treino treino = treinoRepository.postTreino(new Treino(idAluno, treinoRequest));
         log.info("[finish] -> TreinoApplicationService -> postTreino");
-        return null;
+        return new TreinoResponse(treino.getIdTreino());
     }
 }
--- src/main/java/br/com/arenagym/alunotreino/treino/domain/Treino.java
+++ src/main/java/br/com/arenagym/alunotreino/treino/domain/Treino.java
@@ -1,6 +1,8 @@
 package br.com.arenagym.alunotreino.treino.domain;
 
+import br.com.arenagym.alunotreino.treino.application.api.TreinoRequest;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -18,6 +20,8 @@ public class Treino {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "idTreino", updatable = false, unique = true, nullable = false)
     private UUID idTreino;
+    @NotNull
+    private UUID idAlunoRef;
     @NotBlank
     private String nomeTreino;
     @Enumerated(EnumType.STRING)
@@ -27,5 +31,13 @@ public class Treino {
 
     private LocalDateTime dataHoraDoCadastro;
     private LocalDateTime dataHoraDaAtualizacao;
+
+    public Treino(UUID idAluno, @Valid TreinoRequest treinoRequest){
+        this.idAlunoRef = idAluno;
+        this.nomeTreino = treinoRequest.getPeriodico();
+        this.exercicio = treinoRequest.getExercicio();
+        this.dataInicio = treinoRequest.getDataInicio();
+        this.dataHoraDoCadastro = LocalDateTime.now();
+    }
 
 }
